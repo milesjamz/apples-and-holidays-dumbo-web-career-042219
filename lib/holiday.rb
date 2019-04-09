149a72d01394b42b4464db82b53708d5d563cee8
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 15:59:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize! << ":"
+  holiday.each do 
 end
 
 def all_holidays_with_bbq(holiday_hash)
