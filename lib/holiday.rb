6cfdb25e118ffda58c3730062bb72a58ac564054
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 15:59:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts season.to_s.
+  puts season.to_s.capitalize!
 end
 
 def all_holidays_with_bbq(holiday_hash)
